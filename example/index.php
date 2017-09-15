18aa704e783072dc7abdf9bc2cafc9c8c31fd311
Andrew Stilliard <andrew.stilliard@gmail.com>
2017-09-15 23:19:11 +0100

    updated example url

--- example/index.php
+++ example/index.php
@@ -7,6 +7,6 @@ require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 echo \Ditto\Factory::run(array(
 	'proxy_url' => 'http://localhost:8008',
-	'domain_url' => 'http://www.giantstrides.org.uk',
-	'start_path' => '/Crocs_Electro__RedNavy--product--199.html'
+	'domain_url' => 'https://www.postboxshop.com/',
+	'start_path' => '/'
 ));
