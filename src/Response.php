b1f53f3dadc912ad7ab34568aed172f0e62aa474
Andrew Stilliard <andrew.stilliard@gmail.com>
2018-04-30 23:00:05 +0100

    fix: fixed proxying urls with prefixing slash

--- src/Response.php
+++ src/Response.php
@@ -12,12 +12,12 @@ class Response
 	{
 		$this->html = $this->origHtml = $html;
 	}
-	
+
 	public function setProxyPath($path)
 	{
 		$this->proxyPath = $path;
 	}
-	
+
 	public function replaceDomainLinks($domain)
 	{
 		$this->html = str_replace($domain, $this->proxyPath, $this->html);
@@ -27,8 +27,11 @@ class Response
 	{
 		// replace href & src links where they DO NOT start with https?:... or //...
 		$this->html = preg_replace_callback('/(src|href)=(["\'])(?!((["\'])?https?:|(["\'])?\/\/))(.*?)\2/i', function ($matches) use ($usingUrlParam) {
-			return $matches[1] . '=' . $matches[2] . rtrim($this->proxyPath, '/') .'/'. ltrim($usingUrlParam ? urlencode(htmlspecialchars_decode($matches[6])) : $matches[6], '/') . $matches[2];
-		}, $this->html);
+            return $matches[1] . '=' . $matches[2]
+                . rtrim($this->proxyPath, '/')
+                . urlencode('/' . ltrim($usingUrlParam ? htmlspecialchars_decode($matches[6]) : $matches[6], '/'))
+                . $matches[2];
+        }, $this->html);
 	}
 
 	public function replaceInternalCssLinks($usingUrlParam=false)
